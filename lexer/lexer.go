54dacc24bc495874d8fb8c7074acc4fda34d072f
Marius Kimmina <mar.kimmina@gmail.com>
2023-05-12 08:50:11 +0200

    implement readchar function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -13,3 +13,13 @@ func New(input string) *Lexer {
 	}
 	return l
 }
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
