2e402d3df0607a1960fdb2410c4ee230a68570a1
Marius Kimmina <mar.kimmina@gmail.com>
2023-05-13 08:38:13 +0200

    handle numbers and whitespace

--- token/token.go
+++ token/token.go
@@ -31,3 +31,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent checks if the identifier is a keyword
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
